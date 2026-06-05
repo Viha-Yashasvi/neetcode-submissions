b8814ea70affab3ecb3cb6e559c2c964583df62c
Viha-Yashasvi <vihayashasvi@gmail.com>
2026-06-05 13:12:40 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+bool compareIntervals(const Interval& a , const Interval& b){
+        return a.start<b.start;
+}
+    
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        
+        if(intervals.empty()) return true;
+
+        sort(intervals.begin(),intervals.end(),compareIntervals);
+
+        for(int i=0;i<intervals.size()-1;i++){
+            if(intervals[i].end>intervals[i+1].start){
+                return false;
+            }
+        }
+
+        return true;
+        
+    }
+};
